62b04fbc00952c3ef54572a76a80e68da9053fce
MoonNight285 <kimjyjh11@naver.com>
2022-11-24 16:35:10 +0900

    페이징 처리를 위한 모델 추가

--- src/main/java/com/bitc/pagetest/mapper/PageMapper.java
+++ src/main/java/com/bitc/pagetest/mapper/PageMapper.java
@@ -1,6 +1,7 @@
 package com.bitc.pagetest.mapper;
 
 import com.bitc.pagetest.dto.CustomersDto;
+import com.github.pagehelper.Page;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 @Mapper
 public interface PageMapper {
     List<CustomersDto> selectCustomerList() throws Exception;
+    
+    Page<CustomersDto> selectCustomerListPage() throws Exception;
 }
